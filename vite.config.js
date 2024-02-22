bf09d8ea958775fff844c93d3c3b7d155a96db3e
raphaelagil <raphaela.gil@yahoo.com>
2024-02-22 17:53:54 -0300

    changed the defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'build'
-  },
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/React-App-Portfolio/";
+  }
+
+  return config;
+});
