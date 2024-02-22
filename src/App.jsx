fd5a9d2e0f7f087affaa865134adbc46bbe783f0
raphaelagil <raphaela.gil@yahoo.com>
2024-02-22 18:36:56 -0300

    left as / only in router basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
 
   return (
-    <Router basename={`/homework/React`}>
+    <Router basename={`/`}>
       <MyNav />
       <Routes>
         <Route path='/portfolio' element={<ProjectGallery />} />
