0325f794fc65e848386afbeb657720c75437e47b
raphaelagil <raphaela.gil@yahoo.com>
2024-02-23 11:12:45 -0300

    added carrousel for the projects

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import CV from "../../assets/RaphaeladoAmaralGil.pdf"
-
+import './index.css'
 function MyNavBar() {
   return (
     <>
--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,13 +1,10 @@
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React from "react";
+import { Card, Row, Col, Button, Carousel } from "react-bootstrap";
 import projects from "../../projects.json";
 import { useState } from "react"; // Import useState hook
-import { Button } from "react-bootstrap";
 import PageWrapper from "../PageWrapper/Index";
 
 const Projects = () => {
-  // Use an object to store hover state for each button
   const [hoverStates, setHoverStates] = useState({});
 
   const handleMouseEnter = (buttonId) => {
@@ -27,7 +24,7 @@ const Projects = () => {
   return (
     <>
       <PageWrapper>
-        <h3 className="text-center text-light-emphasis">P R O J E C T S <br></br>G A L L E R Y </h3>
+        <h3 className="text-center text-light-emphasis">P R O J E C T S <br />G A L L E R Y </h3>
         <p className="text-light-emphasis">______________________________</p>        
         <div className="text-center m-3">
           <p className="text-light-emphasis">
@@ -40,75 +37,51 @@ const Projects = () => {
             exploration.
           </p>
         </div>
-        <Row xs={1} sm={3} md={2} className="g-4">
+        <Carousel>
           {projects.map((project) => (
-            <Col key={project.id}>
-              <Card
-                className="h-100"
-                style={{ minWidth: "180px",  backgroundColor: "#ea5555"}}
-              >
-                <Card.Body>
-                  <Card.Title
-                    style={{
-                      padding: "5px",
-                      color: "#ffe7e7",
-                      borderRadius: "15px",
-                    }}
-                  >
-                    {project.title}
-                  </Card.Title>
-                  <Card.Text
-                  style={{color:"#ffe7e7"}}
-                  >{project.pitch}</Card.Text>
-                </Card.Body>
-
-                <Card.Img
-                  variant="top"
-                  src={project.image}
-                  style={{
-                    height: "300px",
-                    objectFit: "fill",
-                    // borderTop: "2px solid #547258",
-                    // borderBottom: "2px solid #547258",
-                  }}
-                />
-
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                  <Button
-                    variant="success"
-                    href={project.deployedLink}
-                    style={{
-                      border: "none",
-                      margin: "10px",
-                      color: hoverStates[`deployedLink_${project.id}`] ?"white" : "#ea5555",
-                      backgroundColor: hoverStates[`deployedLink_${project.id}`] ? "#f6c6c2" : '#f6c6c2',
-                      transition: "color 0.3s ease"
-                    }}
-                    onMouseEnter={() => handleMouseEnter(`deployedLink_${project.id}`)} // Set hover state to true on mouse enter
-                    onMouseLeave={() => handleMouseLeave(`deployedLink_${project.id}`)} // Set hover state to false on mouse leave
-                  >
-                    Deployed app
-                  </Button>
-                  <Button
-                    variant="success"
-                    href={project.repoLink}
-                    style={{
-                      border: "none",
-                      margin: "10px",
-                      color: hoverStates[`repoLink_${project.id}`] ?"white" : "#ea5555",
-                      backgroundColor: hoverStates[`repoLink_${project.id}`] ? "#f6c6c2" : '#f6c6c2',
-                      transition: "color 0.3s ease"
-                    }}
-                    onMouseEnter={() => handleMouseEnter(`repoLink_${project.id}`)} // Set hover state to true on mouse enter
-                    onMouseLeave={() => handleMouseLeave(`repoLink_${project.id}`)} // Set hover state to false on mouse leave
-                  >
-                    GitHub repo
-                  </Button>
-                </div>
-              </Card>
-            </Col>
+            <Carousel.Item key={project.id}>
+                <Col>
+                  <Card className="h-100" style={{ backgroundColor: "#ea5555" }}>
+                  <Card.Img className='d-block w-70' variant="top" src={project.image} style={{ height: "500px", objectFit: "fill" }} />
+                      <Card.Title style={{ padding: "5px", color: "#ffe7e7", borderRadius: "15px" }}>{project.title}</Card.Title>                    
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                      <Button
+                        variant="success"
+                        href={project.deployedLink}
+                        style={{
+                          border: "none",
+                          margin: "10px",
+                          color: hoverStates[`deployedLink_${project.id}`] ? "white" : "#ea5555",
+                          backgroundColor: hoverStates[`deployedLink_${project.id}`] ? "#f6c6c2" : "#f6c6c2",
+                          transition: "color 0.3s ease",
+                        }}
+                        onMouseEnter={() => handleMouseEnter(`deployedLink_${project.id}`)}
+                        onMouseLeave={() => handleMouseLeave(`deployedLink_${project.id}`)}
+                      >
+                        Deployed app
+                      </Button>
+                      <Button
+                        variant="success"
+                        href={project.repoLink}
+                        style={{
+                          border: "none",
+                          margin: "10px",
+                          color: hoverStates[`repoLink_${project.id}`] ? "white" : "#ea5555",
+                          backgroundColor: hoverStates[`repoLink_${project.id}`] ? "#f6c6c2" : "#f6c6c2",
+                          transition: "color 0.3s ease",
+                        }}
+                        onMouseEnter={() => handleMouseEnter(`repoLink_${project.id}`)}
+                        onMouseLeave={() => handleMouseLeave(`repoLink_${project.id}`)}
+                      >
+                        GitHub repo
+                      </Button>
+                    </div>
+                  </Card>
+                </Col>
+      
+            </Carousel.Item>
           ))}
-        </Row>
+        </Carousel>
       </PageWrapper>
     </>
   );
