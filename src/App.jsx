ae1a77d106af34006d3330aef10a01314a375fe7
raphaelagil <raphaela.gil@yahoo.com>
2024-03-21 12:40:14 +0000

    did the npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,59 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
-import {  About, Contact, Custom404, Home, Projects } from './components/Pages';
-import PageWrapper from "./components/PageWrapper/Index"
-import './App.css'
+import { About, Contact, Custom404, Home, Projects } from './components/Pages';
+import PageWrapper from "./components/PageWrapper/Index";
+import './App.css';
 
 function App() {
-
   return (
-    <BrowserRouter
-    basename={import.meta.env.DEV ? "/" : "/Portfolio-React/"}
-  >
-    <MyNav />
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <PageWrapper>
-            <Home />
-          </PageWrapper>
-        }
-      />
-      <Route
-        path="about"
-        element={
-          <PageWrapper>
-            <About />
-          </PageWrapper>
-        }
-      />
-      <Route
-        path="/projects"
-        element={
-          <PageWrapper>
-            <Projects />
-          </PageWrapper>
-        }
-      />
-      <Route
-        path="/404"
-        element={
-          <PageWrapper>
-            <Custom404 />
-          </PageWrapper>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <PageWrapper>
-            <Contact />
-          </PageWrapper>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Portfolio-React/"}>
+      <MyNav />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PageWrapper>
+              <Home />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageWrapper>
+              <About />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <PageWrapper>
+              <Projects />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <PageWrapper>
+              <Contact />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <PageWrapper>
+              <Custom404 />
+            </PageWrapper>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
