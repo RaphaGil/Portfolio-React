ccfdf22996fbf0ca8d25c630f614148bfe6ca5c4
raphaelagil <raphaela.gil@yahoo.com>
2024-05-14 21:36:19 +0100

    added skills in my home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons"; // Import the arrow-up icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import myImage from '../../assets/img/me.png';
-import PageWrapper from "../PageWrapper/Index";
-import FooterBtns from "../FooterBtns/Index";
+import PageWrapper from '../PageWrapper/Index';
+import FooterBtns from '../FooterBtns/Index';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSkillIndex: 0,
+      skills: ['React', 'Next.js', 'JavaScript', 'Bootstrap', 'Node.js']
+    };
+  }
+
+  componentDidMount() {
+    // Start interval to change skills every 3 seconds
+    this.skillInterval = setInterval(this.changeSkill, 3000);
+  }
+
+  componentWillUnmount() {
+    // Clear interval when component is unmounted to prevent memory leaks
+    clearInterval(this.skillInterval);
+  }
+
+  changeSkill = () => {
+    // Update currentSkillIndex to cycle through skills array
+    const { currentSkillIndex, skills } = this.state;
+    const newIndex = (currentSkillIndex + 1) % skills.length;
+    this.setState({ currentSkillIndex: newIndex });
+  };
+
   scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' 
+      behavior: 'smooth',
     });
   };
 
   render() {
+    const { skills, currentSkillIndex } = this.state;
+    const currentSkill = skills[currentSkillIndex];
+
     return (
       <>
         <PageWrapper>
@@ -26,21 +54,34 @@ class Home extends React.Component {
                 alt="Professional profile picture of a person crossing arms"
               />
               <div className="body">
-                <h1 className=" mt-4 mt-3 text-light-emphasis">Olá!</h1>
-                <p className="text-light-emphasis" style={{fontSize: '26px'}}>
+                <h1 className="mt-4 mt-3 text-light-emphasis">Olá!</h1>
+                <p className="text-light-emphasis" style={{ fontSize: '26px' }}>
                   I am <b>Raphaela Gil</b> &#128075; and I am a Frontend Web Developer based in London, UK
                 </p>
-                <p className="text-light-emphasis" style={{fontSize: '20px'}}>
-                  Welcome to my <b>portfolio</b>. This is my webpage where I share side projects that I've been working on.
+                <p className="text-light-emphasis" style={{ fontSize: '20px' }}>
+                  Welcome to my <b>portfolio</b>. This is my webpage where I share side projects that I've been working on in <b style={{ fontSize: '26px' }}>{currentSkill}</b>.
                 </p>
               </div>
             </div>
           </section>
         </PageWrapper>
-        <div className='p-4'>
+        <div className="p-4">
           <FooterBtns />
         </div>
-        <button onClick={this.scrollToTop} style={{ position: 'fixed', bottom: '20px', right: '20px', padding: '10px', borderRadius: '50%', border: 'none', backgroundColor: 'transparent', color: '#ea5555', cursor: 'pointer' }}>
+        <button
+          onClick={this.scrollToTop}
+          style={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            padding: '10px',
+            borderRadius: '50%',
+            border: 'none',
+            backgroundColor: 'transparent',
+            color: '#ea5555',
+            cursor: 'pointer',
+          }}
+        >
           <FontAwesomeIcon icon={faArrowUp} /> {/* Use the FontAwesomeIcon component with the arrow-up icon */}
         </button>
       </>
