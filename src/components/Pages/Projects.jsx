eb2fe9ba4c25cc787f7edfc03582059bc8325c01
raphaelagil <raphaela.gil@yahoo.com>
2025-02-21 09:55:03 +0000

    added height for the div and images portfolio

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -20,80 +20,66 @@ const Projects = () => {
   return (
     <>
       <PageWrapper>
-        <div className="text-center " style={{ fontSize: "24px", border:'none' }}>
-          <h3 className="text-light-emphasis mt-4">
-            P R O J E C T S
-          </h3>
-        
+        <div className="text-center" style={{ fontSize: "24px", border: "none" }}>
+          <h3 className="text-light-emphasis mt-4">P R O J E C T S</h3>
         </div>
         <Row>
           {projects.map((project) => (
             <Col lg={6} key={project.id}>
               <Card
-  className="mt-5 position-relative shadow-lg border-none"
-  style={{
-    borderRadius: "25px",
-    border: "none",
-    width: "100%",
-    height: "100%", // Ensure the card takes full height
-    display: "flex",
-    flexDirection: "column",
-  }}
-  onMouseEnter={() => handleMouseEnter(project.id)}
-  onMouseLeave={handleMouseLeave}
->
-  <Card.Img
-    className="d-block border-none"
-    src={project.image}
-    style={{
-      borderBlockColor: "white",
-      borderRadius: "25px",
-      height: "100%", // Ensure the image fills the card
-      objectFit: "cover", // Keeps image proportional
-    }}
-  />
-
+                className="mt-5 position-relative shadow-lg border-none"
+                style={{
+                  borderRadius: "25px",
+                  border: "none",
+                  width: "100%",
+                  height: "350px", // Fixed height for card
+                  overflow: "hidden",
+                }}
+                onMouseEnter={() => handleMouseEnter(project.id)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <Card.Img
+                  className="d-block border-none"
+                  src={project.image}
+                  style={{
+                    borderRadius: "25px",
+                    height: "350px", // Fixed height for image
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                />
                 {hoveredCard && hoveredCard.id === project.id && (
                   <div
-                    className="position-absolute w-100 d-flex justify-content-center align-items-center  border-none"
-                    style={{ backgroundColor: "white", borderRadius: "25px", height:'100%' }}
+                    className="position-absolute w-100 d-flex justify-content-center align-items-center border-none"
+                    style={{
+                      backgroundColor: "white",
+                      borderRadius: "25px",
+                      height: "350px", // Fixed height for overlay
+                      top: "0",
+                      left: "0",
+                    }}
                   >
-                    <Card.Body
-                      className="text-center"
-                    >
+                    <Card.Body className="text-center">
                       <Card.Title
                         style={{
                           color: "#ea5555",
                           fontWeight: "bold",
-                          borderBlockColor:'white',
                         }}
                       >
                         {project.title}
                       </Card.Title>
 
-                      <Card.Text
-                        style={{
-                          color: "black"
-                        }}
-                      >
-                        {project.pitch}
-                      </Card.Text>
-                      <div
-                        style={{display: "flex", justifyContent: "center", padding:'10px' }}
-                      >
+                      <Card.Text style={{ color: "black" }}>{project.pitch}</Card.Text>
+                      <div style={{ display: "flex", justifyContent: "center", padding: "10px" }}>
                         <Button
                           href={project.deployedLink}
                           style={{
                             marginRight: "10px",
                             backgroundColor: "white",
                             border: "none",
-                            // width: '10%'
                           }}
                         >
-                          <FontAwesomeIcon
-                            icon={faGlobe}
-                            style={{ color: "#ea5555", height: "100%", paddingRight: '5px',}}
-                          />
+                          <FontAwesomeIcon icon={faGlobe} style={{ color: "#ea5555" }} />
                         </Button>
 
                         <Button
@@ -103,12 +89,8 @@ const Projects = () => {
                             backgroundColor: "white",
                             border: "none",
                           }}
-                          className="github-link"
                         >
-                          <FontAwesomeIcon
-                            icon={faGithub}
-                            style={{ color: "#ea5555", height: "100%", padding: 'px'}}
-                          />
+                          <FontAwesomeIcon icon={faGithub} style={{ color: "#ea5555" }} />
                         </Button>
                       </div>
                     </Card.Body>
